285f826a0f856cc270a684596b5b6a984129ec06
sudhanshu <mandharesudhanshu@gmail.com>
2025-03-19 22:20:38 +0530

    Here's my first commit of CalJs

---
+++ index.js
@@ -0,0 +1,20 @@
+display = document.getElementById("display");
+
+function AddToDisplay(text){
+    display.value += text;
+}
+
+function ClearDisplay(){
+    display.value = "";
+}
+
+function Evaluate() {
+    try{
+        display.value = (eval(display.value));
+    }
+    catch(err)
+    {
+        display.value = "error";
+    }
+    
+}
